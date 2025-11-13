5acd5baadff5946fc48a7563639edcd9783e8e23
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-11-13 23:24:24 +0700

    style: glassmorphism

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="mx-20 px-10 py-5 mt-7 flex justify-between items-center">
+    <nav className="mx-20 px-10 py-6 mt-7 flex justify-between items-center sticky top-3 z-50 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 backdrop-saturate-100 backdrop-contrast-100">
       <p className="text-xl font-semibold">RasaKetintang</p>
       <div className=""></div>
     </nav>
