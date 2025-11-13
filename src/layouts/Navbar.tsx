35daad3c03121309b09dc06c42a68fffd6837cb6
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-11-13 22:48:22 +0700

    chore: change website name

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="mx-20 px-10 py-5 mt-7 flex justify-between items-center">
-      <p className="text-2xl font-semibold">UMKM Ketintang</p>
+      <p className="text-xl font-semibold">RasaKetintang</p>
       <div className=""></div>
     </nav>
   );
