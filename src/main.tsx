261c4d11e7603ea2e2e59b9e31e00fac75ecd0f1
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-11-13 21:40:07 +0700

    chore: update import paths and styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import './index.css'
-import App from './App.tsx'
+import "./assets/css/index.css";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
