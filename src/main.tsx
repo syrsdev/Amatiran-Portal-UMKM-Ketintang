ed574bc0ef8a96d8530b4ce3227068742a3e4aac
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-11-16 11:46:46 +0700

    chore: add route

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,21 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import "./assets/css/index.css";
 import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Search from "./pages/Search";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/detail/:id",
+    element: <Detail />,
+  },
+  {
+    path: "/search",
+    element: <Search />,
   },
 ]);
 
